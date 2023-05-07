3e67168ab3e93587263540819c19447d2f9a1879
Muhammed Nidhal <94860874+MuhammedNidhal@users.noreply.github.com>
2023-05-07 14:39:58 +0300

    fully customizing admin panel

--- KPI/admin.py
+++ KPI/admin.py
@@ -1,6 +1,58 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import *
 from KPI.models import *
 
-admin.site.register(Employee)
+admin.site.site_header = "KPI PROJECT PANEL"
+admin.site.site_title = "KPI PROJECT PANEL"
+
+
+class EmployeeDetails(admin.ModelAdmin):
+    fields = (
+        "name",
+        "department",
+        "tone_of_voice",
+        "starting_script",
+        "ending_script",
+        "resolution_time",
+        "choice_of_words",
+        "clarity",
+        "onCallComplaintChecking",
+        "handlingUnderPressure",
+        "ticketRaising",
+        "billableTime",
+    )
+    list_display = [
+        "name",
+        "department",
+        "monthly_rating",
+        "last_month_rating",
+        "displaymonth",
+    ]
+    list_display_links = [
+        "name",
+    ]
+    list_filter = ("name", "monthly_rating", "department")
+    search_fields = ("name",)
+
+    
+
+    def displaymonth(self, obj):
+        kpis = [
+            obj.tone_of_voice,
+            obj.starting_script,
+            obj.ending_script,
+            obj.resolution_time,
+            obj.choice_of_words,
+            obj.clarity,
+            obj.onCallComplaintChecking,
+            obj.handlingUnderPressure,
+            obj.ticketRaising,
+            obj.billableTime,
+        ]
+        return "{}".format(round(sum(kpis) / len(kpis), 1))
+
+
+admin.site.register(Employee, EmployeeDetails)
 admin.site.register(Department)
+admin.site.register(Address)
+admin.site.unregister(Group)
--- KPI/models.py
+++ KPI/models.py
@@ -3,26 +3,39 @@ from django.db import models
 
 
 class Department(models.Model):
-    id = models.AutoField("id",auto_created=True,unique=True,primary_key=True)
     name = models.CharField("name",max_length=255)
     number_of_employees = models.IntegerField("number of employees",null=True,blank=True)
     
     def __str__(self):
         return self.name
-
-
 class Employee(models.Model):
-    id = models.AutoField("id",auto_created=True,unique=True,primary_key=True)
-    name = models.CharField("name",max_length=255)
+    name = models.CharField("name",max_length=255, null=True, blank=True)
     department = models.ForeignKey(Department,max_length=255,default="CC INBOUND",on_delete=models.CASCADE)
-    tone_of_voice = models.FloatField("tone of voice",null=True,blank=True)
-    starting_script = models.FloatField("starting script",null=True,blank=True)
-    ending_script = models.FloatField("ending script",null=True,blank=True)
-    solution_time = models.FloatField("solution time",null=True,blank=True)
-    choice_of_words = models.FloatField("choice of words",null=True,blank=True)
-    monthly_rating = models.FloatField("monthly rating",null=True,blank=True)
-    prediction = models.FloatField("prediction",null=True,blank=True)
-    last_month_rating = models.FloatField("last month", null=True,blank=True)
+    
+    tone_of_voice = models.FloatField("tone of voice",null=False,blank=False, default=0.0)
+    starting_script = models.FloatField("starting script",null=False,blank=False, default=0.0)
+    ending_script = models.FloatField("ending script",null=False,blank=False, default=0.0)
+    resolution_time = models.FloatField("solution time",null=False,blank=False, default=0.0)
+    choice_of_words = models.FloatField("choice of words",null=False,blank=False, default=0.0)
+    clarity = models.FloatField("clarity", null=False, blank=False, default=0.0)
+    onCallComplaintChecking = models.FloatField("on call complaint checking", null=False, blank=False, default=0.0)
+    handlingUnderPressure = models.FloatField("handling under pressure", null=False, blank=False, default=0.0)
+    ticketRaising = models.FloatField("ticket raising", null=False, blank=False, default=0.0)
+    billableTime = models.FloatField("billable time", null=False, blank=False, default=0.0)
+
+    monthly_rating = models.FloatField("monthly rating",null=False,blank=False, default=0.0)
+    last_month_rating = models.FloatField("last month", null=False,blank=False, default=0.0)
+    time_of_eval = models.DateTimeField("time of evaluation", auto_now=True)
     
     def __str__(self):
         return self.name
+
+class Address(models.Model):
+    name = models.CharField("name", max_length=255, null=False, blank=False)
+    user = models.ForeignKey(Employee,max_length=255,on_delete=models.CASCADE)
+    Address1 = models.CharField("address1", max_length=255, null=False, blank=True)
+    Address2 = models.CharField("address2", max_length=255, null=False, blank=True)
+    houseNumber = models.CharField("houseNumber", max_length=255, null=True, blank=True)
+
+
+    
